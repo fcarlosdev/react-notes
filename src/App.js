b99238eebac82d6cecf5dd052bb9cc7f1150e7f1
fcarlosdev <fcarlosbr@gmail.com>
2020-03-02 23:06:51 -0300

    Refactoring the app.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React, { useRef, useState } from "react";
 import "./App.css";
+import AddNote from "./components/AddNote";
+import NoteList from "./components/NoteList";
 
 function App() {
   const [notes, setNotes] = useState([]);
-  const [title, setTitle] = useState("");
-  const [content, setContent] = useState("");
 
   const titleRef = useRef(null);
   const contentRef = useRef(null);
@@ -21,71 +21,33 @@ function App() {
   };
 
   document.addEventListener("click", e => {
-    if (
-      e.target.classList[0] === "App" ||
-      e.target.getAttribute("id") === "bt-close"
-    ) {
-      controlFormResize("close");
-    } else {
-      controlFormResize("open");
-    }
+    
+    let action = (e.target.classList[0] === "App" || 
+                  e.target.getAttribute("id") === "bt-close") ? "close" : "open";
+
+    controlFormResize(action);
   });
 
-  const handleSubmit = e => {
-    e.preventDefault();
-    if (title || content) {
-      setNotes([...notes,{id: (notes.length+1), title, content}])
+  const handleSubmit = note => {
+    if (note.title || note.content) {
+      note.id = (notes.length + 1);
+      setNotes([...notes, note]);
     }
-    setTitle("");
-    setContent("");
     contentRef.current.style.height = "auto";
   };
 
-  const handleTitleChange = () => {
-    setTitle(titleRef.current.value)
-  }
-
-  const handleContentChange = () => {
-    setContent(contentRef.current.value);
-  }
 
   return (
     <div className="App">
-      <form className="form-note" onSubmit={handleSubmit}>
-        <input
-          typ="text"
-          name="title"
-          className="field"
-          placeholder="Title"
-          ref={titleRef}
-          value={title}
-          onChange={handleTitleChange}
-        />
-        <textarea
-          name="content"
-          className="field"
-          rows="1"
-          placeholder="Enter a note"
-          ref={contentRef}
-          value={content}
-          onChange={handleContentChange}
-          onInput={ () => {
-            contentRef.current.style.height = "auto";
-            contentRef.current.style.height = contentRef.current.scrollHeight + "px";
-          }}
-        ></textarea>
-        <div className="form-footer" ref={footerRef}>
-          <button id="bt-close">close</button>
-        </div>
-      </form>
-      <div className="notes">
-        {notes.map(note => {
-          return (<div className="note" key={note.id}> 
-            <div className="note-title">{note.title}</div>
-            <div className="note-title">{note.content}</div>
-          </div>)
-        })}
-      </div>
+      <AddNote
+        submit={handleSubmit}
+        refs={{
+          titleRef: titleRef,
+          contentRef: contentRef,
+          footerRef: footerRef
+        }}
+      />
+      <NoteList notes={notes}/>
     </div>
   );
 }
